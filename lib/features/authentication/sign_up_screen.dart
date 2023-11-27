c4de980626dd56b2914bde5dbb2f8b2495574ecf
viva-lee <phoenix71405@gmail.com>
2023-11-27 23:09:49 +0900

    Username Screen

--- lib/features/authentication/sign_up_screen.dart
+++ lib/features/authentication/sign_up_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
-import 'package:tiktok_clone/features/authentication/email_screen.dart';
+import 'package:tiktok_clone/features/authentication/username_screen.dart';
 import 'package:tiktok_clone/features/authentication/login_screen.dart';
 import 'package:tiktok_clone/features/authentication/widgets/auth_button.dart';
 
@@ -17,10 +17,10 @@ class SignUpScreen extends StatelessWidget {
     );
   }
 
-  void _onEmailTap(BuildContext context) {
+  void _onUsernameTap(BuildContext context) {
     Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (context) => const EmailScreen(),
+        builder: (context) => const UsernameScreen(),
       ),
     );
   }
@@ -54,7 +54,7 @@ class SignUpScreen extends StatelessWidget {
               ),
               Gaps.v40,
               GestureDetector(
-                onTap: () => _onEmailTap(context),
+                onTap: () => _onUsernameTap(context),
                 child: const AuthButton(
                     icon: FaIcon(FontAwesomeIcons.user),
                     text: "Use email & password"),
