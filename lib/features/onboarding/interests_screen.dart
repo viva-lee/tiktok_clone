1c984cb2516eb35e47fef4af6dfd04bc4d12e3ba
viva-lee <phoenix71405@gmail.com>
2023-12-01 22:34:21 +0900

    Interests Screen

--- lib/features/onboarding/interests_screen.dart
+++ lib/features/onboarding/interests_screen.dart
@@ -1,10 +1,135 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 class InterestsScreen extends StatelessWidget {
   const InterestsScreen({super.key});
+  static const interests = [
+    "Daily life",
+    "Comedy",
+    "Entertainment",
+    "Animals",
+    "Food",
+    "Drama",
+    "Beauty & Style",
+    "Learning",
+    "Sports",
+    "Arts & Crafts",
+    "Daily life",
+    "Comedy",
+    "Entertainment",
+    "Animals",
+    "Food",
+    "Drama",
+    "Beauty & Style",
+    "Learning",
+    "Sports",
+    "Arts & Crafts",
+    "Daily life",
+    "Comedy",
+    "Entertainment",
+    "Animals",
+    "Food",
+    "Drama",
+    "Beauty & Style",
+    "Learning",
+    "Sports",
+    "Arts & Crafts",
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Choose your interests",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size24,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Gaps.v32,
+              const Text(
+                "Choose your interests",
+                style: TextStyle(
+                  fontSize: Sizes.size40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Gaps.v20,
+              const Text(
+                "Get better video recommendations",
+                style: TextStyle(
+                  fontSize: Sizes.size20,
+                ),
+              ),
+              Gaps.v64,
+              Wrap(
+                runSpacing: 10,
+                spacing: 10,
+                children: [
+                  for (var interest in interests)
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: Sizes.size16,
+                        horizontal: Sizes.size24,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(
+                          Sizes.size32,
+                        ),
+                        border: Border.all(
+                          color: Colors.black.withOpacity(0.1),
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.05),
+                            blurRadius: 5,
+                            spreadRadius: 5,
+                          ),
+                        ],
+                      ),
+                      child: Text(
+                        interest,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        elevation: 2,
+        child: Container(
+          padding: const EdgeInsets.symmetric(
+            vertical: Sizes.size10,
+          ),
+          decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor,
+          ),
+          child: const Text(
+            "Next",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: Sizes.size20,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
